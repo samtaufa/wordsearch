89ed97d2b4eb32dddaebe8d8c48c52aec8e15f11
Samiuela LV Taufa <samt@nomoa.com>
2009-05-27 23:50:32 +1000

    html styling   - improve html styling, especially for article output   - makes it so html output is usefully formatted for     either direct printing or importation into another     tool (such as word-processor)

--- src/wordsearch.py
+++ src/wordsearch.py
@@ -47,32 +47,133 @@ class wordsearch:
         matrixsize = self.wsmatrix.maxrow * self.wsmatrix.maxcol
         matrixratio = int(spotfilled / float(matrixsize) * 100)
         puzzlestats = "%s words (%s of %s cells spot filled [%s%%])"%(wordcount, spotfilled, matrixsize, matrixratio)
-        puzzledirections = "Find words hidden: %s" % self.wsmatrix.wsDir
+        puzzledirections = "<div class='ws-directions'>Find words hidden: %s</div>" % self.wsmatrix.wsDir
         
         myarticle = ""
         puzzlepage = ""
         if self.format == 'html':
             wsformat.html()
             myarticle = ""
+            template_stylesheet="""
+        <style>
+        <!--
+         /* Font Definitions */
+            @font-face
+                   {font-family:Cambria;
+                   panose-1:2 4 5 3 5 4 6 3 2 4;}
+            @font-face
+                   {font-family:Calibri;
+                   panose-1:2 15 5 2 2 2 4 3 2 4;}
+            @font-face
+                   {font-family:Consolas;
+                   panose-1:2 11 6 9 2 2 4 3 2 4;}
+         /* Style Definitions */
+            p {
+                margin-right:0cm;
+                margin-left:0cm;
+                font-size:12.0pt;
+                font-family:"Times New Roman","serif";
+            }
+            p.ws-puzzle, li.ws-puzzle, div.ws-puzzle {
+                margin:0cm;
+                margin-bottom:.0001pt;
+                text-align:center;
+                font-size:20.0pt;
+                font-family:"Courier New";
+            }
+            p.ws-wordlist, li.ws-wordlist, div.ws-wordlist {
+                margin-top:24.0pt;
+                margin-right:0cm;
+                margin-bottom:0cm;
+                margin-left:0cm;
+                margin-bottom:.0001pt;
+                text-align:justify;
+                font-size:14.0pt;
+                font-family:"Calibri","sans-serif";
+            }
+            p.ws-solution, li.ws-solution, div.ws-solution {
+                margin:0cm;
+                margin-bottom:.0001pt;
+                text-align:right;
+                font-size:8.0pt;
+                font-family:"Courier New";
+            }
+            p.ws-article, li.ws-article, div.ws-article {
+                margin:0cm;
+                margin-bottom:.0001pt;
+                font-size:14.0pt;
+                font-family:"Times New Roman","serif";
+            }
+            div.ws-article {
+                margin-top: 12pt;
+            }
+            p.title, li.title, div.title {
+                margin-top:0cm;
+                margin-right:0cm;
+                margin-bottom:15.0pt;
+                margin-left:0cm;
+                page-break-after:avoid;
+                border:none;
+                padding:0cm;
+                font-size:26.0pt;
+                font-family:"Cambria","serif";
+                color:#17365D;
+                letter-spacing:.25pt;
+            }
+            p.subtitle, li.subtitle, div.subtitle {
+                margin:0cm;
+                margin-bottom:.0001pt;
+                page-break-after:avoid;
+                font-size:12.0pt;
+                font-family:"Cambria","serif";
+                color:#4F81BD;
+                letter-spacing:.75pt;
+                font-style:italic;
+            }
+            .highlight {
+                font-style: italic;
+                font-weight: bold;
+            }
+            p.ws-directions, li.ws-directions, div.ws-directions {
+                margin:0cm;
+                margin-bottom:.0001pt;
+                page-break-after:avoid;
+                font-size:12.0pt;
+                font-family:"Cambria","serif";
+                #color:#4F81BD;
+                letter-spacing:.75pt;
+                font-style:italic;
+            }
+            
+        -->
+        </style>            
+            """
+            template_body="""
+    <body>
+        <p class='title'>Title: Word Search Puzzle</p>
+        <p class='subtitle'>Sub-Title</p>
+        <p class='directions'>%s</p>
+        <div class='ws-puzzle'>
+        %s
+        </div>
+        <div class='ws-wordlist'>
+        %s
+        </div>
+    """
+            
+            template_head = """
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Wordsearch Puzzle Generation</title>
+        %s
+    </head>
+                """ % template_stylesheet
             template = """
-                    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-                        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-                    <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-                    <head>
-                    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-                        <title>Wordsearch Puzzle Generation</title>
-                    </head>
-                    <body>
-                    <p class='title'>Title: Word Search Puzzle</p>
-                    <p class='subtitle'>Sub-Title</p>
-                    <p class='directions'>%s</p>
-                    <div class='ws-puzzle'>
-                    %s
-                    </div>
-                    <div class='ws-wordlist'>
-                    %s
-                    </div>
-                """
+%s
+%s""" % (template_head, template_body)
             puzzlepage = template % (puzzledirections,
                                      wsformat.matrixFormatted,
                                      wsformat.acceptedFormatted)
